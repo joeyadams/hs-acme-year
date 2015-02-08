4b8f7151633186e3b2624a12a478f8909e8d3423
Joey Adams <joeyadams3.14159@gmail.com>
2015-02-08 17:11:48 -0500

    Fix benchmark to support criterion 1.0.0.0, where Benchmarkable is no longer a typeclass

--- bench/Main.hs
+++ bench/Main.hs
@@ -3,6 +3,6 @@ import Criterion.Main
 
 main :: IO ()
 main = defaultMain
-         [ bench "control"     (return 1234        :: IO Int)
-         , bench "currentYear" (return currentYear :: IO Int)
+         [ bench "control"     $ nfIO (return 1234        :: IO Int)
+         , bench "currentYear" $ nfIO (return currentYear :: IO Int)
          ]
